14ac8baa8433583ad7d1a46c1b9f11c8c61a9cdc
Codeworm08 <siddhantmajumder@gmail.com>
2023-04-15 19:24:36 +0530

    solved set2-2

--- DSA/02_29_02.c
+++ DSA/02_29_02.c
@@ -1,87 +1,165 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct Node{
+struct Node
+{
   int coeff;
   int pow;
-  struct Node* next;
+  struct Node *next;
 };
-typedef struct Node* node;
-void createNode(int c, int p, node*temp)
+typedef struct Node *node;
+
+void createNode(int c, int p, node *temp)
 {
-  node r,t;
-  r=(node)malloc(sizeof(struct Node));
-  r->next=NULL;
-  r->coeff=c;
-  r->pow=p;
-  
-  if (*temp==NULL){
-    *temp=r;
+  node r, t;
+  r = (node)malloc(sizeof(struct Node));
+  r->next = NULL;
+  r->coeff = c;
+  r->pow = p;
+
+  if (*temp == NULL)
+  {
+    *temp = r;
   }
-  else {
-      t=*temp;
-    while (t->next!=NULL)
-      t=t->next;
-    t->next=r;
+  else
+  {
+    t = *temp;
+    while (t->next != NULL)
+      t = t->next;
+    t->next = r;
   }
 }
 
-void sort(node* head)
+void createPoly(node *head)
 {
-  node l,h;
-  int p,c;
-  if(*head==NULL)
+  int x,c,p;
+
+  do {
+    printf("\nEnter power: ");
+    scanf("%d",&p);
+    printf("\nEnter coefficient: ");
+    scanf("%d",&c);
+    createNode(c, p, head);
+    printf("\nEnter 0 to Stop.");
+    scanf("%d",&x);
+  }while (x!=0);
+}
+void sort(node *head)
+{
+  node l, h;
+  int p, c;
+  if (*head == NULL)
   {
-    printf("\nEmpty List!!");
     return;
   }
-  l=*head;
-  if(l->next==NULL)
+  l = *head;
+  if (l->next == NULL)
   {
     return;
-  } 
-  while(l!=NULL)
+  }
+  while (l != NULL)
   {
-    h=l->next;
-    while(h!=NULL)
+    h = l->next;
+    while (h != NULL)
     {
-      if(h->pow<l->pow)
+      if (h->pow < l->pow)
       {
-        p=l->pow;
-        c=l->coeff;
-        l->pow=h->pow;
-        h->pow=p;
-        l->coeff=h->coeff;
-        h->coeff=c;
+        p = l->pow;
+        c = l->coeff;
+        l->pow = h->pow;
+        h->pow = p;
+        l->coeff = h->coeff;
+        h->coeff = c;
       }
-      h=h->next;
-    } 
-    l=l->next;
+      h = h->next;
+    }
+    l = l->next;
   }
 }
 void show(node p)
 {
   node t;
-  t=p;
-  if(t==NULL)
+  t = p;
+  if (t == NULL)
     return;
-  while (t!=NULL) {
+  while (t != NULL)
+  {
     printf(" %dx^%d", t->coeff, t->pow);
-    if (t->next != NULL){
-      if (t->next->coeff >=0)
+    if (t->next != NULL)
+    {
+      if (t->next->coeff >= 0)
         printf(" +");
     }
-    t=t->next;
+    t = t->next;
   }
+  printf("\n");
+}
+
+node add(node p1, node p2)
+{
+  node sum, x, y, t;
+  sum = NULL;
+  if (p1 == NULL && p2 == NULL)
+  {
+    printf("No polynomials passed!");
+    return sum;
+  }
+  x = p1;
+  y = p2;
+  sort(&x);
+  sort(&y);
+  show(x);
+  show(y);
+  while (x != NULL && y != NULL)
+  {
+    if (x->pow < y->pow)
+    {
+      createNode(x->coeff, x->pow, &sum);
+      x = x->next;
+    }
+    else if (x->pow > y->pow)
+    {
+      createNode(y->coeff, y->pow, &sum);
+      y = y->next;
+    }
+    else
+    {
+      createNode(y->coeff + x->coeff, y->pow, &sum);
+      x = x->next;
+      y = y->next;
+    }
+  }
+  
+  while (x != NULL)
+  {
+    createNode(x->coeff, x->pow, &sum);
+    x = x->next;
+  }
+  while (y != NULL)
+  {
+    createNode(y->coeff, y->pow, &sum);
+    y = y->next;
+  }
+  return sum;
 }
 
 int main()
 {
-  node p1,p2;
-  p1=NULL;p2=NULL;
-  createNode(3, 2, &p1);
+  node p1, p2, sum;
+  p1 = NULL;
+  p2 = NULL;
+  printf("\nEnter 1st polynomial:");
+  createPoly(&p1);
+  printf("\nEnter 2nd polynomial:");
+  createPoly(&p2);
+  /*createNode(3, 2, &p1);
   createNode(-1, 1, &p1);
-  createNode(4, 3, &p1);
-  sort(&p1);
-  show(p1);
+  createNode(-1, 9, &p1);
+  createNode(5, 3, &p1);
+  createNode(5, 3, &p2);
+  createNode(3, 9, &p2);
+  createNode(4, 3, &p2);*/
+  
+  sum = add(p1, p2);
+  show(sum);
   return 0;
 }
