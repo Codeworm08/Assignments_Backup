873f389198106f2532b51e596a33f5ec0740b467
Codeworm08 <siddhantmajumder@gmail.com>
2023-04-26 23:55:25 +0530

    solved q5 of java set1

---
+++ SEM2/JAVA/Set1/Q5.java
@@ -0,0 +1,23 @@
+package A1;
+
+import java.util.Scanner;
+
+public class Q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner Sc = new Scanner (System.in);
+		System.out.println("Enter string: ");
+		String s1 = Sc.nextLine();
+		String s2 = s1;
+		System.out.println(s1==s2);
+		System.out.println(s1 + ".equals(" + s2 + ")" + s1.equals(s2));
+		System.out.println("Enter 2 strings: ");
+		String s3 = Sc.nextLine();
+		String s4 = Sc.nextLine();
+		System.out.println(s3==s4);
+		System.out.println(s3 + ".equals(" + s4 + ")" +s3.equals(s4));
+		Sc.close();
+	}
+
+}
