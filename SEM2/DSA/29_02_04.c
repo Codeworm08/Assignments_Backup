8ef9019979f10acbca4014f6cb6f33ad50035fe2
Codeworm08 <siddhantmajumder@gmail.com>
2023-05-02 00:53:19 +0530

    Solved DSA set2-4

--- SEM2/DSA/29_02_04.c
+++ SEM2/DSA/29_02_04.c
@@ -1,50 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-int ex2d(int n, int k)
+
+/*
+    i.) 2d Array
+*/
+int **createArr(int n)
 {
-    int people[n][2];
+    int **arr = (int **)calloc(sizeof(int *), 2);
+    for (int i = 0; i < 2; i++)
+        arr[i] = (int *)calloc(sizeof(int), n);
     for (int i = 0; i < n; i++)
-    {
-        people[i][0] = i;
-        people[i][1] = 0;
-    }
-    int current = 0;
-    while (1)
-    {
-        int count = people[current][1]; // number of people to skip
-        for (int i = 0; i < n; i++)
-        {
-            if (people[i][1] == 0)
-            {
-                count--;
-                if (count == 0)
-                {
-                    // execute the next person
-                    people[i][1] = 1;
-                    current = (i + 1) % n;
-                    break;
-                }
-            }
-        }
-        int left = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (people[i][1] == 0)
-                left++;
-        }
-        if (left == 1)
-            break;
-        people[current][1] = k;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        if (people[i][1] == 0)
-        {
-            return i;
-        }
-    }
-    return 0;
+        arr[0][i] = i + 1;
+    return arr;
 }
+int left(int **m, int n)
+{
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (m[1][i] == 0)
+            count++;
+    }
+    return count;
+}
+int ex2d(int **p, int n, int k, int st)
+{
+    int i = st, sk;
+    while (left(p, n) > 1)
+    {
+        while (p[1][i] != 0)
+            i = (i + 1) % n;
+        sk = 0;
+        while (sk <= k)
+        {
+            if (p[1][i] == 0)
+                sk++;
+            i = (i + 1) % n;
+        }
+        //printf("%d",i);
+        while (p[1][i] != 0)
+            i = (i + 1) % n;
+        p[1][i] = 1;
+        //printf("%d=>%d",i,p[1][i]);
+    }
+    for(i =0; i<n;i++)
+    {
+        if(p[1][i]==0)
+            return i;
+    }
+}
+
+/*
+    ii.) Circular Linked List
+*/
 
 struct list
 {
@@ -98,65 +106,12 @@ void display(node head)
     printf("%d", t->data);
 }
 
-void delFirst(node *head)
+int exL(int n, int k, int st, node *head)
 {
-    node p, t;
-    if (*head == NULL)
-    {
-        printf("\nEmpty List!!");
-        return;
-    }
-    p = *head;
-    t = *head;
-    while (t->next != *head)
-        t = t->next;
-    if (t == *head)
-    {
-        *head = NULL;
-        free(t);
-    }
-    // printf("\nElement deleted: %d",head->data);
-    *head = (*head)->next;
-    free(p);
-}
-
-void delK(int k, node *head)
-{
-    node p, t;
     int i;
-    if (*head == NULL)
-    {
-        printf("\nEmpty List!!");
-        return;
-    }
-    if (k == 1)
-    {
-        delFirst(head);
-        return;
-    }
-    p = *head;
-    for (i = 1; i < k - 1; i++)
-    {
-        if (p->next == *head)
-        {
-            printf("\nPosition does not exist in List!");
-            return;
-        }
-        p = p->next;
-    }
-    t = p->next;
-    p->next = t->next;
-    t->next = NULL;
-    free(t);
-}
-
-int exL(int n, int k, node *head)
-{
-    int i, st;
     node r, prev;
     r = *head;
-    printf("\nEnter starting point(0-%d): ", n - 1);
-    scanf("%d", &st);
+
     for (i = 0; i < st; i++)
         r = r->next;
     while ((*head)->next != *head)
@@ -186,6 +141,9 @@ int exL(int n, int k, node *head)
         }
         else
         {
+            prev = *head;
+            while (prev->next != r)
+                prev = prev->next;
             prev->next = r->next;
             r->next = NULL;
             free(r);
@@ -194,17 +152,131 @@ int exL(int n, int k, node *head)
     }
     return (*head)->data;
 }
+
+/*
+    iii.) Circular Doubly Linked List
+*/
+struct dlist
+{
+    int data;
+    struct dlist *left;
+    struct dlist *right;
+};
+typedef struct dlist *dnode;
+dnode createDNode(int i)
+{
+    dnode temp;
+    temp = (dnode)malloc(sizeof(struct dlist));
+    temp->data = i;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+void createDList(int n, dnode *head)
+{
+    dnode temp, rear;
+    *head = NULL;
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        temp = createDNode(i);
+        if (*head == NULL)
+        {
+            *head = temp;
+            rear = *head;
+            rear->right = *head;
+            rear->left = *head;
+        }
+        else
+        {
+            rear->right = temp;
+            temp->left = rear;
+            rear = rear->right;
+            rear->right = *head;
+            (*head)->left = rear;
+        }
+    }
+}
+int exDL(int n, int k, int st, dnode *head)
+{
+    int i;
+    dnode r, prev;
+    r = *head;
+    for (i = 0; i < st; i++)
+        r = r->right;
+    while ((*head)->right != *head)
+    {
+        for (i = 0; i <= k; i++)
+            r = r->right;
+        if (r == *head)
+        {
+            prev = (*head)->left;
+            (*head) = (*head)->right;
+            prev->right = (*head);
+            (*head)->left = prev;
+            r->left = NULL;
+            r->right = NULL;
+            free(r);
+            r = *head;
+        }
+        else if (r->right == *head)
+        {
+            prev = r->left;
+            prev->right = *head;
+            (*head)->left = prev;
+            r->right = NULL;
+            r->left = NULL;
+            free(r);
+            r = *head;
+        }
+        else
+        {
+            prev = r->left;
+            prev->right = r->right;
+            r->right->left = prev;
+            r->left = NULL;
+            r->right = NULL;
+            free(r);
+            r = prev->right;
+        }
+    }
+    return (*head)->data;
+}
 int main()
 {
     node head;
-    int n, k;
-    printf("\nEnter no of people: ");
-    scanf("%d", &n);
-    printf("\nEnter skips: ");
-    scanf("%d", &k);
-    createList(n, &head);
-    display(head);
-    printf("\nPerson alive: %d\n", exL(n, k, &head));
-    // printf("\n%d", ex2d(n, k));
+    dnode dhead;
+    int n, k, st, ch;
+    int **p;
+    do
+    {
+        printf("\nEnter no of people: ");
+        scanf("%d", &n);
+        printf("\nEnter skips: ");
+        scanf("%d", &k);
+        printf("\nEnter starting point(0-%d): ", n - 1);
+        scanf("%d", &st);
+        printf("\n1->2d Array\n2->Circular Linked List\n3->Circular Doubly Linked List\n0->Quit\n\t=>");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            p = createArr(n);
+            printf("\nPerson alive: %d\n", ex2d(p, n, k, st));
+            break;
+        case 2:
+            createList(n, &head);
+            printf("\nPerson alive: %d\n", exL(n, k, st, &head));
+            break;
+        case 3:
+            createDList(n, &dhead);
+            printf("\nPerson alive: %d\n", exDL(n, k, st, &dhead));
+            break;
+        case 0:
+            break;
+        default:
+            printf("\nInvalid choice.");
+        }
+    } while (ch != 0);
     return 0;
 }
