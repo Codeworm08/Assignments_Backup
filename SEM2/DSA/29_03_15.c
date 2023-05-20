8f046031428ae7cc7a07ec0a8449d24a34572dce
Codeworm08 <siddhantmajumder@gmail.com>
2023-05-20 16:19:06 +0530

    Updated qn 15 of DSA Set3

--- SEM2/DSA/29_03_15.c
+++ SEM2/DSA/29_03_15.c
@@ -5,11 +5,11 @@
 typedef struct node{
 	int data;
 	struct node *next;
-}node;
+}NODE;
 
 typedef struct queue{
-	node *rear;
-	node *front;
+	NODE *rear;
+	NODE *front;
 }queue;
 
 void create(queue *q)
@@ -20,9 +20,9 @@ void create(queue *q)
 	return;
 }
 
-node * createnode(int n)
+NODE * createnode(int n)
 {
-	node *c=(node *)malloc(sizeof(node));
+	NODE *c=(NODE *)malloc(sizeof(NODE));
 	c->data=n;
 	c->next=NULL;
 	return c;
@@ -66,4 +66,84 @@ bool getFront(queue *q, int *x)
         return false;
     *x=q->front->data;
     return true;
+}
+
+struct list{
+    int data;
+    struct list *next;
+};
+typedef struct list *node;
+
+node createNode(int data)
+{
+    node new = (node)malloc(sizeof(struct list));
+    new->data = data;
+    new->next = NULL;
+    return new;
+}
+
+void lpush(int n,node *top)
+{
+    node p;
+    if(*top == NULL)
+    {
+        *top=createNode(n);
+        return;
+    }
+    p=createNode(n);
+    p->next=*top;
+    *top=p;
+}
+int lpop(node *top,int *f)
+{
+    if(*top == NULL)
+    {
+        *f=-1;//flag to check Underflow
+        return -1;
+    }
+    node p=*top;
+    int t=(*top)->data;
+    *top=(*top)->next;
+    p->next=NULL;
+    free(p);
+    return t;
+}
+
+int main()
+{
+    int ch,data;
+    queue *q=NULL;
+    NODE *t;
+    do
+    {
+        printf("\n0->Quit\nEnter 1 to enque\nEnter 2 to dequeue\nEnter 3 to getFront\n=> ");
+        scanf("%d",&ch);
+        switch (ch)
+        {
+        case 0:
+            break;
+        case 1:
+            printf("\nEnter data: ");
+            scanf("%d",&data);
+            enqueue(data,q);
+            break;
+        case 2:
+            if(dequeue(q,&data))
+                printf("\nData removed: %d",data);
+            else
+                printf("\nQueue Empty!");
+            break;
+        case 3:
+            if(getFront(q,&data))
+                printf("\nData at front = %d",data);
+            else
+                printf("\nQueue Empty!");
+            break;
+        default:
+            printf("\nInvalid input");
+            break;
+        }
+    } while (ch!=0);
+    
+    return 0;
 }
