8635396c8fa67cc4ce59be4f68cfa88fb9f2e61b
Codeworm08 <siddhantmajumder@gmail.com>
2023-08-11 11:49:22 +0530

    net q2 a1 runs till exit entered

--- SEM3/Net/2client.py
+++ SEM3/Net/2client.py
@@ -2,11 +2,15 @@ import socket
 TCP_PORT = 5000
 IP_ADDR = '127.0.0.1'
 BUF_SIZE = 1024
-exp = input("Enter integer arithmetic expression:")
 k = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 k.connect((IP_ADDR,TCP_PORT))
 print("Client sent connection request")
-k.send(exp.encode())
-res = k.recv(BUF_SIZE)
-print(res.decode())
+while True:
+    exp = input("Enter integer arithmetic expression:")
+    k.send(exp.encode())
+    if exp.lower()=="exit":
+        break
+    res = k.recv(BUF_SIZE)
+    print(res.decode())
+print("Client disconnected")
 k.close()
--- SEM3/Net/2server.py
+++ SEM3/Net/2server.py
@@ -11,16 +11,19 @@ k.listen(1)
 print("Server waiting for client")
 con, addr = k.accept()
 print("Connection address is ", addr)
-data = con.recv(BUF_SIZE)
-print("Received expression: ",data.decode())
-try:
-	res = str(eval(data))
-	res = "Result: "+res
-	con.send(res.encode())
-except Exception as x:
-	print(x)
-	err = "Invalid integer expression: %s"%(x)
-	err=str(err)
-	con.send(err.encode())
-finally:
-	con.close()
+while True:
+	data = con.recv(BUF_SIZE)
+	print("Received expression: ",data.decode())
+	if data.decode().lower() == "exit":
+		break
+	try:
+		res = str(eval(data))
+		res = "Result: "+res
+		con.send(res.encode())
+	except Exception as x:
+		print(x)
+		err = "Invalid integer expression: %s"%(x)
+		err=str(err)
+		con.send(err.encode())
+print("Server closed!")
+con.close()
