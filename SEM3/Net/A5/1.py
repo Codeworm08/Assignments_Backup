8cb92662380f532fdc4bb47935f2940a7c6bcb73
Codeworm08 <siddhantmajumder@gmail.com>
2023-09-06 01:13:46 +0530

    solved network a5 q1

---
+++ SEM3/Net/A5/1.py
@@ -0,0 +1,19 @@
+#from scapy.all import Ether, IP, TCP
+from scapy.all import IP, ICMP, send, sr1
+import ipaddress
+from time import sleep
+networkAddress = input("Enter Network Address: ")
+ipAd = ipaddress.ip_network(networkAddress, False)
+timeout = int(input("Time to wait for reply in seconds:"))
+hosts=[]
+print("Sending ICMP packet to all hosts....")
+for i in ipAd.hosts():
+    #print(i)
+    d=str(i)
+    #print("Sending ICMP packet to ",d,"...")
+    res=sr1(IP(dst=d)/ICMP()/"ABCD", verbose=False,timeout=timeout)
+    if res:
+        hosts.append(i)
+print("Received reply from:")
+for h in hosts:
+    print(h)
